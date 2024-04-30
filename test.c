9122fdbafc22585e4029d92cce67dec530d37874
JenSeop <dostwoman@gmail.com>
2024-04-30 10:39:37 +0900

    test commit-3

--- test.c
+++ test.c
@@ -7,18 +7,18 @@
 int main(void)
 {
     char *str1 = (char*)malloc(STR_LEN * sizeof(char));
-    char *str2 = (char*)malloc((STR_LEN / 6) * sizeof(char));
+    //char *str2 = (char*)malloc((STR_LEN / 6) * sizeof(char));
 
     for(int idx = 0; idx < STR_LEN; idx++)
     {
         str1[idx] = 'a';
-        str2[idx] = 'a';
+        //str2[idx] = 'a';
     }
     str1[STR_LEN] = '\0';
-    str2[STR_LEN / 10] = '\0';
+    //str2[STR_LEN / 10] = '\0';
 
     printf("strlen1 = %d\n",strlen(str1));
     free(str1);
-    printf("strlen2 = %d\n",strlen(str2));
-    free(str2);
+    //printf("strlen2 = %d\n",strlen(str2));
+    //free(str2);
 }

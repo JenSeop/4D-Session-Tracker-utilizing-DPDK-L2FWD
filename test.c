b781290113c45cb108fb64dbc910fe005a88be32
JenSeop <dostwoman@gmail.com>
2024-04-30 10:49:30 +0900

    test commit-4

--- test.c
+++ test.c
@@ -4,21 +4,53 @@
 
 #define STR_LEN 20000000
 
-int main(void)
+char *depth_01;
+char *depth_02;
+
+void depth_01_init()
 {
-    char *str1 = (char*)malloc(STR_LEN * sizeof(char));
-    //char *str2 = (char*)malloc((STR_LEN / 6) * sizeof(char));
+    depth_01 = (char*)malloc(STR_LEN * sizeof(char));
 
     for(int idx = 0; idx < STR_LEN; idx++)
-    {
-        str1[idx] = 'a';
-        //str2[idx] = 'a';
-    }
-    str1[STR_LEN] = '\0';
-    //str2[STR_LEN / 10] = '\0';
+        depth_01[idx] = 'a';
+    
+    depth_01[STR_LEN] = '\0';
+    
+    printf("depth_01_init = %d\n",strlen(depth_01));
+}
 
-    printf("strlen1 = %d\n",strlen(str1));
-    free(str1);
-    //printf("strlen2 = %d\n",strlen(str2));
-    //free(str2);
+void depth_01_free()
+{
+    free(depth_01);
+    printf("depth_01_free\n");
+}
+
+void depth_02_init()
+{
+    depth_02 = (char*)malloc((STR_LEN / 2) * sizeof(char));
+
+    for(int idx = 0; idx < (STR_LEN / 2); idx++)
+        depth_02[idx] = 'a';
+
+    depth_02[STR_LEN / 2] = '\0';
+    
+    printf("depth_02_init = %d\n",strlen(depth_02));
+}
+
+void depth_02_free()
+{
+    free(depth_02);
+    printf("depth_02_free\n");
+}
+
+int main(void)
+{
+    // INIT
+    depth_01_init();
+    depth_02_init();
+    // FREE
+    depth_01_free();
+    depth_02_free();
+
+    return 0;
 }
